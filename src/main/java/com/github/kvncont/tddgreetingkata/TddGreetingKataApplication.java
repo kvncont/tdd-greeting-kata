192966a9124a28e848bc9f9d4f155aa124d68c76
Kevin Dario Contreras Rodriguez <kvncont@gmail.com>
2021-05-26 00:24:36 -0600

    Initial commit

---
+++ src/main/java/com/github/kvncont/tddgreetingkata/TddGreetingKataApplication.java
@@ -0,0 +1,13 @@
+package com.github.kvncont.tddgreetingkata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TddGreetingKataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TddGreetingKataApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/kvncont/tddgreetingkata/TddGreetingKataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.kvncont.tddgreetingkata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TddGreetingKataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
